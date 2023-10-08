b7d7bbeb13c9e607382071b0870ce5e368f3a95d
kushaal <kushaalrajiv@gmail.com>
2023-10-08 19:35:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Cards from './components/Cards'
 import './App.css';
 import image1 from "./image1.png";
 import wedding from "./wedding.png";
-import mountain from "./mountain.png"
+import mountain from "./mountain.png";
+import guitar from './guitar.png';
+import sing from './sing.png';
+import dance from './dance.png';
+import swim from './swim.png';
 
 function App() {
   return (
@@ -19,31 +23,59 @@ function App() {
         lesson="Life lessons with Katie Zaferes"
         reviews='6'
         price='137'
+        country="Israel"
+        avail="SOLD OUT"
       />
 
       <Cards 
         img= {mountain}
         rating= '4.5'
-        lesson="Life lessons with Katie Zaferes"
+        lesson="Learn Wedding Photography"
         reviews='2'
         price='112'
+        country="Bali"
       />
       <Cards 
         img= {wedding}
         rating= '4.8'
-        lesson="Life lessons with Katie Zaferes"
+        lesson="Group Mountian Biking"
         reviews='2'
         price='149'
-
+        country="Vietnam"
       />
       <Cards 
-        img= {image1}
-        rating= '5'
-        lesson="Life lessons with Katie Zaferes"
-
+        img= {guitar}
+        rating= '4.9'
+        lesson="Guitar Lessons"
+        reviews="7"
+        price='130'
+        country="USA"
       />
       
-      
+      <Cards 
+        img= {sing}
+        rating= '4.6'
+        lesson="Singing Class"
+        reviews="4"
+        price='110'
+        country="India"
+      />
+      <Cards 
+        img= {dance}
+        rating= '4.7'
+        lesson="Dance Class"
+        reviews="11"
+        price='170'
+        country="Austria"
+      />
+      <Cards 
+        img= {swim}
+        rating= '4.9'
+        lesson="Swimming Class"
+        reviews="18"
+        price='160'
+        country="Netherlands"
+      />
       
       </div>
       
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,16 +1,19 @@
 import React from "react";
-import Star1 from "../Star1.png"
+import Star1 from "../Star1.png";
+
 
 export default function Cards(props) {
     return(
         <div className="container">
-            <img src={props.img} width="176px" height="235px"/>
+            
+            <img src={props.img} width="176px" height="235px" className="image"/>
+            
             <div className="info">
             <img src={Star1} width="14px" height="14px"/>
             <span>{props.rating}</span>
-            <span className="grey">({props.reviews}) USA</span>
+            <span className="grey">({props.reviews}) {props.country}</span>
             </div>
-            <span>{props.lesson}</span>
+            <span className="review">{props.lesson}</span>
             <span><strong>From ${props.price}</strong> / person</span>
         </div>
     )
